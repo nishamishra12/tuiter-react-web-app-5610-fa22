51ccedc21ba5b1c2de327aa5580c246d2b8469cf
nishamishra12 <nishamishra093@gmail.com>
2022-11-05 03:53:39 -0400

    Initial branch and labs

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
                  element={<HelloWorld/>}/>
           <Route path="/tuiter/*"
                  element={<Tuiter/>}/>
+          <Route path="/*"
+                 element={<Labs/>}/>
         </Routes>
       </div>
     </BrowserRouter>
