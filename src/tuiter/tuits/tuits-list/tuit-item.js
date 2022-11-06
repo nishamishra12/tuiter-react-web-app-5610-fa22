b6e7e9980e4cbc9ec3d9576e5c8191b9cb899e1c
nishamishra12 <nishamishra093@gmail.com>
2022-11-06 03:54:52 -0500

    Deleting Tuit

--- src/tuiter/tuits/tuits-list/tuit-item.js
+++ src/tuiter/tuits/tuits-list/tuit-item.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faCheckCircle } from "@fortawesome/fontawesome-free-solid";
+import { faCheckCircle } from "@fortawesome/fontawesome-free-solid";
 import TuitStatsItem from "../tuit-stats/tuit-stats-item"
+import { useDispatch } from "react-redux";
+import { deleteTuit } from "../../tuits/tuits-reducer";
 const TuitItem = (
     {
         tuit = {}
     }
 ) => {
+    const dispatch = useDispatch();
+    const deleteTuitHandler = (id) => {
+        dispatch(deleteTuit(id));
+    }
+
     return (
         <li class="list-group-item bg-transparent p-1">
             <div class="row">
@@ -15,15 +22,17 @@ const TuitItem = (
                 </div>
                 <div class="col-10 ps-0">
                     <div class="mx-auto">
-                        <span class="fw-bold">{tuit.userName}</span> <FontAwesomeIcon icon={faCheckCircle}/> <span class="text-secondary">{tuit.handle} .
+                        <span class="fw-bold">{tuit.userName}</span> <FontAwesomeIcon icon={faCheckCircle} /> <span class="text-secondary">{tuit.handle} .
                             {tuit.time}</span>
+                        <i className="bi bi-x-lg float-end"
+                            onClick={() => deleteTuitHandler(tuit._id)}></i>
                     </div>
                     <div>{tuit.tuit}</div>
                     <TuitStatsItem
                         key={tuit._id} tuit={tuit} />
                 </div>
             </div>
-            
+
         </li>
     );
 };
