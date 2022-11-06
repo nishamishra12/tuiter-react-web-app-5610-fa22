08766ec2f3d6edc4afdf1bbd69a430e5ad263464
nishamishra12 <nishamishra093@gmail.com>
2022-11-06 01:40:55 -0400

    what's happening txt area

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,33 +1,12 @@
 import React from "react";
-import PostList from "../post-list";
 import "./index.css";
 import TuitsList from "../tuits/tuits-list";
+import WhatsHappening from "./whats-happening";
 const HomeComponent = () => {
     return (
         <>
-            {/* <div className="row">
-                <div className="col-11 position-relative">
-                    <input placeholder="Search Tuiter"
-                        className="form-control rounded-pill ps-5" />
-                    <i className="bi bi-search position-absolute 
-                       wd-nudge-up"></i>
-                </div>
-                <div className="col-1">
-                    <i className="wd-bottom-4 text-primary float-end bi 
-                       bi-gear-fill fs-2 position-relative"></i>
-                </div>
-            </div> */}
-            {/* <ul className="nav nav-pills mb-2">
-                <li className="nav-item">
-                    <a className="nav-link active">For You</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link">Trending</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link">News</a>
-                </li>
-            </ul> */}
+            <h4>Home</h4>
+            <WhatsHappening />
             <TuitsList />
         </>
     );
